5cec294e1e6ba676e9a8b24f29dc62805b1bf542
Leonardo <leonardoplaza70@hotmail.com>
2023-04-19 01:40:03 -0300

    agegue cosas al js

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
 console.log("Hello world!")
+
+
+
+git add para agregar
+git status para ver archivos sin guardar
+git commit para guardar Todo
+.gitignore es un archivo para decirle cosas a ignorar
